40363b153308255a6d1ddfd45717154723bfdd2d
{kmanago} <{martaisa.manago@gmail.com}>
2019-09-25 20:31:01 -0400

    print roles and remove

---
+++ commands/roles/removerole.js
@@ -0,0 +1,25 @@
+module.exports = {
+	name: 'removerole',
+  description: 'Removes a role to the user.',
+  category: 'Roles',
+  args: true,
+  execute(message, args) {
+		if (!args[0]) {
+        message.channel.send('No arguments were added!');
+    } 
+    else {
+        let cmd = args[0];
+        let role = message.guild.roles.find(r => r.name === cmd);
+        if(message.member.roles.some(r=>(r.name === role)) ) {
+            // has one of the roles
+            console.log("user has role");
+          } else {
+            // has none of the roles
+          }
+        //message.member.addRole(role);
+        //message.channel.send("You have been given the new role: " + cmd);
+    }
+
+  },
+  
+};
--- commands/roles/roles.js
+++ commands/roles/roles.js
@@ -1,12 +1,16 @@
+const config = require('../../config.json');
 module.exports = {
 name: 'roles',
   description: 'Retrieves all possible roles within the server.',
   category: 'Roles',
-  args: true,
   execute(message, args) {
 		if (!args[0]) {
-            
-        }    
+      // Get the Guild and store it under the variable "list"
+      const list = message.client.guilds.get(config.guildID); 
+      //list.members.forEach(member => console.log(member.user.username)); 
+      console.log(list.roles);
+      list.roles.forEach(role => message.channel.send(role.name)); 
+    }    
   },
   
 };
